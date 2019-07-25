3a451ed7b4b869ab33d351b626281cd65f7679f0
Anthony Platanios <e.a.platanios@gmail.com>
2019-07-25 12:51:44 -0400

    [SWIFT] Split up the simulation server in its own class.

--- api/swift/Sources/JellyBeanWorld/Simulator.swift
+++ api/swift/Sources/JellyBeanWorld/Simulator.swift
@@ -46,13 +46,16 @@ public enum TurnDirection: UInt32 {
   case front = 0, back, left, right
 }
 
-/// Conflict resolution policy for when multiple agents request to move to the same location.
+/// Conflict resolution policy for when two or more agents request to move to the same location.
 public enum MoveConflictPolicy: UInt32 {
   case noCollisions = 0, firstComeFirstServe, random
 }
 
-/// Policy that determines whether specific actions are allowed, disallowed, or simply allowed but
-/// ignored by the simulator (i.e., nothing happens if an ignored action is requested).
+/// Policy used to indicate whether an action is allowed, disallowed, or ignored. If an action is
+/// disallowed, then attempting to perform it will immediately fail, preventing the simulator from
+/// progressing if an agent has not performed an action during the current time step. If an action
+/// is ignored, then the agent will perform a no-op for that time step (i.e., the simulator will
+/// simply ignore that action).
 public enum ActionPolicy: UInt32 {
   case allowed = 0, disallowed, ignored
 }
@@ -62,18 +65,11 @@ public final class Simulator {
   /// Simulator configuration.
   public let configuration: Configuration
 
-  /// Simulation server configuration. This is `nil` if this is not a simulation server.
-  public let serverConfiguration: ServerConfiguration?
-
   /// Number of simulation steps that have been executed so far.
   public private(set) var time: UInt64 = 0
 
   /// Pointer to the underlying C API simulator instance.
   @usableFromInline internal var handle: UnsafeMutableRawPointer?
-
-  /// Pointer to the underlying C API simulator server instance. This is `nil` if this is not a
-  /// simulation server.
-  @usableFromInline internal var serverHandle: UnsafeMutableRawPointer?
 
   /// States of the agents managed by this simulator (keyed by the agents' unique identifiers).
   @usableFromInline internal var agentStates: [UInt64: AgentState] = [:]
@@ -93,25 +89,12 @@ public final class Simulator {
   ///
   /// - Parameters:
   ///   - configuration: Configuration for the new simulator.
-  ///   - serverConfiguration: Optional server configuration. If this is provided, then the new
-  ///     simulator will also act as a simulation server.
-  public init(
-    using configuration: Configuration,
-    serverConfiguration: ServerConfiguration? = nil
-  ) {
+  public init(using configuration: Configuration) {
     self.configuration = configuration
-    self.serverConfiguration = serverConfiguration
     var cConfig = configuration.toC()
     defer { cConfig.deallocate() }
     self.handle = simulatorCreate(&cConfig.configuration, nativeOnStepCallback)
     simulatorSetStepCallbackData(handle, Unmanaged.passUnretained(self).toOpaque())
-    if let config = serverConfiguration {
-      self.serverHandle = simulationServerStart(
-        handle,
-        config.port,
-        config.connectionQueueCapacity,
-        config.workerCount)
-    }
   }
 
   /// Loads a simulator from the provided file.
@@ -119,20 +102,13 @@ public final class Simulator {
   /// - Parameters:
   ///   - file: File in which the simulator is saved.
   ///   - agents: Agents that this simulator manages.
-  ///   - serverConfiguration: Optional server configuration. If this is provided, then the loaded
-  ///     simulator will also act as a simulation server.
   /// - Precondition: The number of agents provided must match the number of agents the simulator
   ///   managed before its state was saved.
-  public init(
-    fromFile file: URL,
-    agents: [Agent],
-    serverConfiguration: ServerConfiguration? = nil
-  ) {
+  public init(fromFile file: URL, agents: [Agent]) {
     let cSimulatorInfo = simulatorLoad(file.absoluteString, nativeOnStepCallback)
     defer { simulatorDeleteSimulatorInfo(cSimulatorInfo) }
     self.handle = cSimulatorInfo.handle
     self.configuration = Configuration(fromC: cSimulatorInfo.config)
-    self.serverConfiguration = serverConfiguration
     self.time = cSimulatorInfo.time
     self.agentStates = [UInt64: AgentState](
       uniqueKeysWithValues: UnsafeBufferPointer(
@@ -147,18 +123,10 @@ public final class Simulator {
       """)
     self.agents = [UInt64: Agent](uniqueKeysWithValues: zip(agentStates.keys, agents))
     simulatorSetStepCallbackData(handle, Unmanaged.passUnretained(self).toOpaque())
-    if let config = serverConfiguration {
-      self.serverHandle = simulationServerStart(
-        handle,
-        config.port,
-        config.connectionQueueCapacity,
-        config.workerCount)
-    }
   }
 
   deinit {
-    if let h = serverHandle { simulationServerStop(h) }
-    simulatorDelete(handle)
+    if let h = handle { simulatorDelete(h) }
   }
 
   /// Adds a new agent to this simulator, and updates
@@ -246,80 +214,6 @@ public final class Simulator {
       simulator.agentStates[state.id] = AgentState(fromC: state, for: simulator)
     }
     simulator.dispatchSemaphore?.signal()
-  }
-}
-
-/// Simulation agent. Agents can interact with the Jelly Bean World by being added to simulators
-/// that manage them.
-public protocol Agent {
-  /// Returns the agent's desired next action. This function is called automatically by all
-  /// simulators managing this agent, whenever the `Simulator.step()` function is invoked.
-  ///
-  /// - Parameter state: Current state of the agent.
-  /// - Returns: Action requested by the agent.
-  ///
-  /// - Note: The action is not actually performed until the simulator advances by a time step and
-  ///   issues a notification about that event. The simulator only advances by a time step only
-  ///   once all agents have requested some action.
-  mutating func act(using state: AgentState) -> Action
-}
-
-/// State of an agent in the Jelly Bean World.
-public struct AgentState {
-  /// Position of the agent.
-  public let position: Position
-
-  /// Direction in which the agent is facing.
-  public let direction: Direction
-
-  /// Scent that the agent smells. This is a vector with size `S`, where `S` is the scent vector
-  /// size (i.e., the scent dimensionality).
-  public let scent: ShapedArray<Float>
-
-  /// Visual field of the agent. This is a matrix with shape `[V + 1, V + 1, C]`, where `V` is the
-  /// visual range of the agent and `C` is the color vector size (i.e., the color dimensionality).
-  public let vision: ShapedArray<Float>
-
-  /// Items that collected by the agent so far represented as a dictionary mapping items to counts.
-  public let items: [Item: Int]
-}
-
-/// Action that can be taken by agents in the jelly bean world.
-public enum Action {
-  /// No action.
-  case none
-
-  /// Move action, along the specified direction and for the provided number of steps.
-  case move(direction: Direction, stepCount: Int = 1)
-
-  /// Turn action (without any movement to a different cell).
-  case turn(direction: TurnDirection)
-
-  /// Invokes this action in the simulator.
-  ///
-  /// - Parameters:
-  ///   - simulatorHandle: Pointer to the C simulator instance.
-  ///   - clientHandle: Pointer to the C simulator client instance (if a client is being used).
-  ///   - agentID: Identifier of the agent for which to invoke this action.
-  @inlinable
-  internal func invoke(
-    simulatorHandle: UnsafeMutableRawPointer?,
-    clientHandle: UnsafeMutableRawPointer?,
-    agentID: UInt64
-  ) {
-    switch self {
-    case .none:
-      simulatorNoOpAgent(simulatorHandle, clientHandle, agentID)
-    case let .move(direction, stepCount):
-      simulatorMoveAgent(
-        simulatorHandle,
-        clientHandle,
-        agentID,
-        direction.toC(),
-        UInt32(stepCount))
-    case let .turn(direction):
-      simulatorTurnAgent(simulatorHandle, clientHandle, agentID, direction.toC())
-    }
   }
 }
 
@@ -413,9 +307,48 @@ extension Simulator {
       self.removedItemLifetime = removedItemLifetime
     }
   }
+}
 
+/// Jelly Bean World (JBW) simulation server.
+public final class SimulationServer {
+  /// Simulation server configuration. This is `nil` if this is not a simulation server.
+  public let configuration: Configuration
+
+  /// Simulator instance used by this server.
+  public let simulator: Simulator
+
+  /// Pointer to the underlying C API simulation server instance.
+  @usableFromInline internal var handle: UnsafeMutableRawPointer?
+
+  /// Creates a new simulation server.
+  ///
+  /// - Parameters:
+  ///   - configuration: Simulation server configuration.
+  ///   - simulator: Simulator instance used by the simulation server.
+  public init(using configuration: Configuration, simulator: Simulator) {
+    self.configuration = configuration
+    self.simulator = simulator
+    self.handle = simulationServerStart(
+      simulator.handle,
+      configuration.port,
+      configuration.connectionQueueCapacity,
+      configuration.workerCount)
+  }
+
+  deinit {
+    if let h = handle { simulationServerStop(h) }
+  }
+
+  /// Stops this simulations server.
+  @inlinable
+  public func stop() {
+    if let h = handle { simulationServerStop(h) }
+  }
+}
+
+extension SimulationServer {
   /// Simulation server configuration.
-  public struct ServerConfiguration {
+  public struct Configuration {
     /// Port in which the simulation server will be listening.
     public let port: UInt32
 
@@ -430,6 +363,80 @@ extension Simulator {
       self.port = port
       self.connectionQueueCapacity = connectionQueueCapacity
       self.workerCount = workerCount
+    }
+  }
+}
+
+/// Simulation agent. Agents can interact with the Jelly Bean World by being added to simulators
+/// that manage them.
+public protocol Agent {
+  /// Returns the agent's desired next action. This function is called automatically by all
+  /// simulators managing this agent, whenever the `Simulator.step()` function is invoked.
+  ///
+  /// - Parameter state: Current state of the agent.
+  /// - Returns: Action requested by the agent.
+  ///
+  /// - Note: The action is not actually performed until the simulator advances by a time step and
+  ///   issues a notification about that event. The simulator only advances by a time step only
+  ///   once all agents have requested some action.
+  mutating func act(using state: AgentState) -> Action
+}
+
+/// State of an agent in the Jelly Bean World.
+public struct AgentState {
+  /// Position of the agent.
+  public let position: Position
+
+  /// Direction in which the agent is facing.
+  public let direction: Direction
+
+  /// Scent that the agent smells. This is a vector with size `S`, where `S` is the scent vector
+  /// size (i.e., the scent dimensionality).
+  public let scent: ShapedArray<Float>
+
+  /// Visual field of the agent. This is a matrix with shape `[V + 1, V + 1, C]`, where `V` is the
+  /// visual range of the agent and `C` is the color vector size (i.e., the color dimensionality).
+  public let vision: ShapedArray<Float>
+
+  /// Items that collected by the agent so far represented as a dictionary mapping items to counts.
+  public let items: [Item: Int]
+}
+
+/// Action that can be taken by agents in the jelly bean world.
+public enum Action {
+  /// No action.
+  case none
+
+  /// Move action, along the specified direction and for the provided number of steps.
+  case move(direction: Direction, stepCount: Int = 1)
+
+  /// Turn action (without any movement to a different cell).
+  case turn(direction: TurnDirection)
+
+  /// Invokes this action in the simulator.
+  ///
+  /// - Parameters:
+  ///   - simulatorHandle: Pointer to the C simulator instance.
+  ///   - clientHandle: Pointer to the C simulator client instance (if a client is being used).
+  ///   - agentID: Identifier of the agent for which to invoke this action.
+  @inlinable
+  internal func invoke(
+    simulatorHandle: UnsafeMutableRawPointer?,
+    clientHandle: UnsafeMutableRawPointer?,
+    agentID: UInt64
+  ) {
+    switch self {
+    case .none:
+      simulatorNoOpAgent(simulatorHandle, clientHandle, agentID)
+    case let .move(direction, stepCount):
+      simulatorMoveAgent(
+        simulatorHandle,
+        clientHandle,
+        agentID,
+        direction.toC(),
+        UInt32(stepCount))
+    case let .turn(direction):
+      simulatorTurnAgent(simulatorHandle, clientHandle, agentID, direction.toC())
     }
   }
 }
