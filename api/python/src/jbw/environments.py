ec560736f6737e83e0a5b962804763e6be8fe684
David Tao <ruoyutao@gmail.com>
2021-05-20 02:48:45 +0800

    started to edit config

--- api/python/src/jbw/environments.py
+++ api/python/src/jbw/environments.py
@@ -70,6 +70,60 @@ def make_config():
 		no_op_allowed=False, patch_size=32, mcmc_num_iter=4000, items=items, agent_color=[0.0, 0.0, 1.0], agent_field_of_view=2*pi,
     collision_policy=MovementConflictPolicy.FIRST_COME_FIRST_SERVED, decay_param=0.4, diffusion_param=0.14, deleted_item_lifetime=2000)
 
+def make_v1_config():
+  """
+  This config file matches the config
+  """
+  # specify the item types
+  items = []
+  items.append(Item("banana",    [1.92, 1.76, 0.40], [0.96, 0.88, 0.20], [0, 0, 0, 0], [0, 0, 0, 0], False, 0.0,
+                    intensity_fn=IntensityFunction.CONSTANT, intensity_fn_args=[1.5],
+                    interaction_fns=[
+                        [InteractionFunction.PIECEWISE_BOX, 10.0, 100.0, 0.0, -6.0],      # parameters for interaction between item 0 and item 0
+                        [InteractionFunction.ZERO],                                       # parameters for interaction between item 0 and item 1
+                        [InteractionFunction.PIECEWISE_BOX, 200.0, 0.0, -6.0, -6.0],      # parameters for interaction between item 0 and item 2
+                        [InteractionFunction.PIECEWISE_BOX, 10.0, 200.0, 2.0, -100.0],    # parameters for interaction between item 0 and item 3
+                    ]))
+  items.append(Item("onion",     [1.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0, 0, 0, 0], [0, 0, 0, 0], False, 0.0,
+                    intensity_fn=IntensityFunction.CONSTANT, intensity_fn_args=[-5.0],
+                    interaction_fns=[
+                        [InteractionFunction.PIECEWISE_BOX, 200.0, 0.0, -6.0, -6.0],      # parameters for interaction between item 1 and item 0
+                        [InteractionFunction.ZERO],                                       # parameters for interaction between item 1 and item 1
+                        [InteractionFunction.PIECEWISE_BOX, 200.0, 0.0, -100.0, -100.0],  # parameters for interaction between item 1 and item 2
+                        [InteractionFunction.ZERO]                                        # parameters for interaction between item 1 and item 3
+                    ]))
+  items.append(Item("jellybean", [0.0, 0.0, 1.0], [0.0, 0.0, 1.0], [0, 0, 0, 0], [0, 0, 0, 0], False, 0.0,
+                    intensity_fn=IntensityFunction.CONSTANT, intensity_fn_args=[-5.3],
+                    interaction_fns=[
+                        [InteractionFunction.PIECEWISE_BOX, 10.0, 200.0, 2.0, -100.0],    # parameters for interaction between item 2 and item 0
+                        [InteractionFunction.PIECEWISE_BOX, 200.0, 0.0, -100.0, -100.0],  # parameters for interaction between item 2 and item 1
+                        [InteractionFunction.PIECEWISE_BOX, 10.0, 200.0, 0.0, -6.0],      # parameters for interaction between item 2 and item 2
+                        [InteractionFunction.ZERO]                                        # parameters for interaction between item 2 and item 3
+                    ]))
+  items.append(Item("wall",      [0.0, 0.0, 0.0], [0.5, 0.5, 0.5], [0, 0, 0, 1], [0, 0, 0, 0], True, 0.0,
+                    intensity_fn=IntensityFunction.CONSTANT, intensity_fn_args=[0.0],
+                    interaction_fns=[
+                        [InteractionFunction.ZERO],                                       # parameters for interaction between item 3 and item 0
+                        [InteractionFunction.ZERO],                                       # parameters for interaction between item 3 and item 1
+                        [InteractionFunction.ZERO],                                       # parameters for interaction between item 3 and item 2
+                        [InteractionFunction.CROSS, 10.0, 15.0, 20.0, -200.0, -20.0, 1.0] # parameters for interaction between item 3 and item 3
+                    ]))
+  items.append(Item("truffle",      [8.40, 4.80, 2.60], [0.42, 0.24, 0.13], [0, 0, 0, 0], [0, 0, 0, 0], False, 0.0,
+                    intensity_fn=IntensityFunction.CONSTANT, intensity_fn_args=[0.0],
+                    interaction_fns=[
+                      [InteractionFunction.ZERO],                                       # parameters for interaction between item 3 and item 0
+                      [InteractionFunction.ZERO],                                       # parameters for interaction between item 3 and item 1
+                      [InteractionFunction.ZERO],                                       # parameters for interaction between item 3 and item 2
+                      [InteractionFunction.CROSS, 10.0, 15.0, 20.0, -200.0, -20.0, 1.0] # parameters for interaction between item 3 and item 3
+                    ]))
+  # construct the simulator configuration
+  return SimulatorConfig(max_steps_per_movement=1, vision_range=5,
+                         allowed_movement_directions=[ActionPolicy.ALLOWED, ActionPolicy.DISALLOWED, ActionPolicy.DISALLOWED, ActionPolicy.DISALLOWED],
+                         allowed_turn_directions=[ActionPolicy.DISALLOWED, ActionPolicy.DISALLOWED, ActionPolicy.ALLOWED, ActionPolicy.ALLOWED],
+                         no_op_allowed=False, patch_size=32, mcmc_num_iter=4000, items=items, agent_color=[0.0, 0.0, 1.0], agent_field_of_view=2*pi,
+                         collision_policy=MovementConflictPolicy.FIRST_COME_FIRST_SERVED, decay_param=0.4, diffusion_param=0.14, deleted_item_lifetime=2000)
+
+
 if modules_loaded:
   # Construct the simulator configuration.
   sim_config = make_config()
@@ -92,3 +146,21 @@ if modules_loaded:
         'sim_config': sim_config,
         'reward_fn': reward_fn,
         'render': True})
+
+  sim_v1_config = make_v1_config()
+
+  register(
+      id='JBW-v1',
+      entry_point='jbw.environment:JBWEnv',
+      kwargs={
+          'sim_config': sim_v1_config,
+          'reward_fn': reward_fn,
+          'render': False})
+
+  register(
+    id='JBW-render-v1',
+    entry_point='jbw.environment:JBWEnv',
+    kwargs={
+      'sim_config': sim_v1_config,
+      'reward_fn': reward_fn,
+      'render': True})
